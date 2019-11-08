43d883d40a1a1997c9116b7944f2405bac0db252
nissefar <45756310+nissefar@users.noreply.github.com>
2019-11-08 11:24:28 +0100

    Update NewUserTrigger.sql

--- NewUserTrigger.sql
+++ NewUserTrigger.sql
@@ -5,6 +5,5 @@ as
 BEGIN
 insert into beetails default values
 insert into preferences default values
-select * from inserted
 update hivemember set preferenceid = SCOPE_IDENTITY() where id = (select id from inserted)
 END
