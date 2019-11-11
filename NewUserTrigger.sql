226563e4d519829141e696613fe110f58fa07972
nissefar <45756310+nissefar@users.noreply.github.com>
2019-11-11 21:26:08 +0100

    Update NewUserTrigger.sql
    Beetails blev før fejlagtigt oprettet med hivememberid == null
    Dette er rettet

--- NewUserTrigger.sql
+++ NewUserTrigger.sql
@@ -3,7 +3,7 @@ on hivemember
 after insert
 as
 BEGIN
-insert into beetails default values
+insert into beetails (hivememberid) values ((select id from inserted))
 insert into preferences default values
 update hivemember set preferenceid = SCOPE_IDENTITY() where id = (select id from inserted)
 END
