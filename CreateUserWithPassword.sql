577eea23784b5baa648099e965eb5e5182f9f134
nissefar <45756310+nissefar@users.noreply.github.com>
2019-11-12 12:39:45 +0100

    Update CreateUserWithPassword.sql
    jobtitle included

--- CreateUserWithPassword.sql
+++ CreateUserWithPassword.sql
@@ -1,8 +1,8 @@
-CREATE PROCEDURE CreateUserWithLogin @usertypeid int, @genderid int, @firstname nvarchar(50), @lastname nvarchar(50),@email nvarchar(100), @birthdate date, @pass nvarchar(30)
+CREATE PROCEDURE CreateUserWithLogin @usertypeid int, @genderid int, @firstname nvarchar(50), @lastname nvarchar(50),@email nvarchar(100), @birthdate date, @jobtitle string, @pass nvarchar(30)
 AS
 declare @success bit
 BEGIN TRY
-INSERT INTO hivemember (usertypeid, genderid, firstname, lastname, email, birthdate) values (@usertypeid, @genderid,@firstname,@lastname,@email,@birthdate)
+INSERT INTO hivemember (usertypeid, genderid, firstname, lastname, email, birthdate, jobtitle) values (@usertypeid, @genderid,@firstname,@lastname,@email,@birthdate,@jobtitle)
 INSERT INTO userlogin (userid, pass) values (SCOPE_IDENTITY(), @pass)
 SET @success = 1
 END TRY
