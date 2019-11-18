ec4fc586134573ec5610971a0cbf6b5de816779b
annadavidsen <anna.m.davidsen@gmail.com>
2019-11-18 09:57:45 +0100

    started work on ui

---
+++ Buzzers/BuzzerGui/Startup/ViewModelInstaller.cs
@@ -0,0 +1,21 @@
+﻿using BuzzerGui.ViewModels;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuzzerGui.Startup
+{
+    public class ViewModelInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            //container.Register(Component.For<IMainWindowViewModel>()
+            //    .ImplementedBy<MainWindowViewModel>());
+        }
+    }
+}
---
+++ Buzzers/BuzzerGui/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Application;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuzzerGui.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private IAccountManager _manager;
+
+        private string _email;
+        private string _password;
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                _email = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DelegateCommand LogInCommand { get; private set; }
+
+        public MainWindowViewModel(IAccountManager manager)
+        {
+            _manager = manager;
+            
+            LogInCommand = new DelegateCommand(() => LogIn());
+        }
+
+        private void LogIn()
+        {
+            var member = _manager.Login(_email, _password);
+        }
+    }
+}
