e30c02f5f006ff406803c629b1d973aa54f91ebd
annadavidsen <anna.m.davidsen@gmail.com>
2019-11-22 09:08:23 +0100

    asdfghjklæl

--- Buzzers/BuzzerGui/ViewModels/LoginViewModel.cs
+++ Buzzers/BuzzerGui/ViewModels/LoginViewModel.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Application;
+using BuzzerGui.Utility;
+using Domain;
+using Prism.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +10,58 @@ using System.Threading.Tasks;
 
 namespace BuzzerGui.ViewModels
 {
-    public class LoginViewModel : ViewModelBase
+    public class LoginViewModel : ViewModelBase, INavigationViewModel
     {
+        private IAccountManager _manager;
+
+        private string _email;
+        private string _password;
+
+        public MemberStory Story { get; set; }
+
         public LoginViewModel()
         {
+        }
 
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                _email = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DelegateCommand LogInCommand { get; private set; }
+
+        public LoginViewModel(IAccountManager manager)
+        {
+            _manager = manager;
+
+            LogInCommand = new DelegateCommand(() => LogIn());
+            Story = _manager.GetMemberStory();
+
+        }
+
+        private void LogIn()
+        {
+            var member = _manager.Login(_email, _password);
+            if (member != null)
+            {
+
+                Messenger.Default.Send(member);
+            }
         }
     }
 }
